080f854b773fc0ee29858568bc2c2e8328cc5ed9
jonathan0900 <abcdej30035@gmail.com>
2025-07-22 23:28:32 +0800

    Update 1-1.cpp

--- HW1/1-1.cpp
+++ HW1/1-1.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int ackermann(int a, int b) {
+    if (a == 0) return b + 1;
+    else if (b == 0) return ackermann(a - 1, 1);
+    else return ackermann(a - 1, ackermann(a, b - 1));
+}
+
+int main() {
+    int a = 0, b = 1;
+    cout << "A(" << a << "," << b << ") = " << ackermann(a, b) << endl;
+    return 0;
+}
